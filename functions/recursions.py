c0704540881102989208fa0fa151437d3daff5d0
hariprasadche <hariprasad.che@bmsce.ac.in>
2026-01-29 15:03:41 +0530

    recurssion is added

---
+++ functions/recursions.py
@@ -0,0 +1,24 @@
+# def function(n):
+#     if n == 0:          # base case
+#         return
+#     print("Hello World")
+#     function(n-1)
+## function(5)
+
+
+# def function(count):
+#     count +=1
+#     print("Hai Hari")
+#     if count < 15:
+#         function(count)
+#     else:
+#         return
+#
+# function(12)
+
+
+def function():
+    print("Hello World")
+    function()
+
+function()
